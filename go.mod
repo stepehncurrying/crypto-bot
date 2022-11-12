e13a874a85b0cd9694faf39f705d2f42873c66f4
ateston1 <ateston@fi.uba.ar>
2022-11-12 19:58:21 -0300

    Read .env variables

---
+++ go.mod
@@ -0,0 +1,5 @@
+module crypto-bot
+
+go 1.13
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	// Load Env variables from .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error reading environment variables")
+		return
+	}
+	fmt.Println(os.Getenv("SLACK_AUTH_TOKEN"))
 }
