0d025e934a99951d9b9fd5668cbdd6748508b5ab
ateston1 <ateston@fi.uba.ar>
2022-11-12 21:20:29 -0300

    Send a message to channel

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module crypto-bot
 
 go 1.13
 
-require github.com/joho/godotenv v1.4.0 // indirect
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/slack-go/slack v0.11.4 // indirect
+)
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
 	"os"
+	"time"
 )
 
 func main() {
@@ -13,5 +15,34 @@ func main() {
 		fmt.Println("Error reading environment variables")
 		return
 	}
-	fmt.Println(os.Getenv("SLACK_AUTH_TOKEN"))
+
+	token := os.Getenv("SLACK_AUTH_TOKEN")
+	channel_id := os.Getenv("SLACK_CHANNEL_ID")
+	client := slack.New(token, slack.OptionDebug(true))
+
+	attachment := slack.Attachment{
+		Pretext: "Super Bot Message",
+		Text:    "some text",
+		// Color Styles the Text, making it possible to have like Warnings etc.
+		Color: "#36a64f",
+		// Fields are Optional extra data!
+		Fields: []slack.AttachmentField{
+			{
+				Title: "Date",
+				Value: time.Now().String(),
+			},
+		},
+	}
+
+	_, timestamp, err := client.PostMessage(
+		channel_id,
+		// uncomment the item below to add a extra Header to the message, try it out :)
+		//slack.MsgOptionText("New message from bot", false),
+		slack.MsgOptionAttachments(attachment),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Message sent at %s", timestamp)
 }
