d4b82c445463885b2016838b3bd3587a396aa997
ateston1 <ateston@fi.uba.ar>
2022-11-13 01:17:13 -0300

    Add bot commands

--- main.go
+++ main.go
@@ -1,48 +1,173 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/joho/godotenv"
 	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+	"github.com/slack-go/slack/socketmode"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
 	// Load Env variables from .env file
 	err := godotenv.Load(".env")
+
 	if err != nil {
-		fmt.Println("Error reading environment variables")
+		fmt.Println("Error: ", err)
 		return
 	}
 
 	token := os.Getenv("SLACK_AUTH_TOKEN")
-	channel_id := os.Getenv("SLACK_CHANNEL_ID")
-	client := slack.New(token, slack.OptionDebug(true))
-
-	attachment := slack.Attachment{
-		Pretext: "Super Bot Message",
-		Text:    "some text",
-		// Color Styles the Text, making it possible to have like Warnings etc.
-		Color: "#36a64f",
-		// Fields are Optional extra data!
-		Fields: []slack.AttachmentField{
-			{
-				Title: "Date",
-				Value: time.Now().String(),
-			},
-		},
-	}
-
-	_, timestamp, err := client.PostMessage(
-		channel_id,
-		// uncomment the item below to add a extra Header to the message, try it out :)
-		//slack.MsgOptionText("New message from bot", false),
-		slack.MsgOptionAttachments(attachment),
-	)
+	appToken := os.Getenv("SLACK_APP_TOKEN")
+	//channelId := os.Getenv("SLACK_CHANNEL_ID")
+	api := slack.New(token, slack.OptionDebug(true), slack.OptionAppLevelToken(appToken))
+	client := socketmode.New(api, socketmode.OptionDebug(false))
 
 	if err != nil {
-		panic(err)
+		fmt.Println("Error:", err)
+		return
 	}
-	fmt.Printf("Message sent at %s", timestamp)
+
+	// Create a context that can be used to cancel goroutine
+	ctx, cancel := context.WithCancel(context.Background())
+	// Make this cancel called properly in a real program , graceful shutdown etc
+	defer cancel()
+
+	go func(ctx context.Context, api *slack.Client, socketClient *socketmode.Client) {
+		// Create a for loop that selects either the context cancellation or the events incomming
+		for {
+			select {
+			// inscase context cancel is called exit the goroutine
+			case <-ctx.Done():
+				log.Println("Shutting down socketmode listener")
+				return
+			case event := <-socketClient.Events:
+				// We have a new Events, let's type switch the event
+				// Add more use cases here if you want to listen to other events.
+				switch event.Type {
+				// handle EventAPI events
+				case socketmode.EventTypeEventsAPI:
+					// The Event sent on the channel is not the same as the EventAPI events so we need to type cast it
+					eventsAPIEvent, ok := event.Data.(slackevents.EventsAPIEvent)
+					if !ok {
+						log.Printf("Could not type cast the event to the EventsAPIEvent: %v\n", event)
+						continue
+					}
+					// We need to send an Acknowledge to the slack server
+					socketClient.Ack(*event.Request)
+					// Now we have an Events API event, but this event type can in turn be many types, so we actually need another type switch
+					err := handleEventMessage(eventsAPIEvent, api)
+
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+			}
+		}
+	}(ctx, api, client)
+
+	err = client.Run()
+}
+
+func handleEventMessage(event slackevents.EventsAPIEvent, api *slack.Client) error {
+	switch event.Type {
+	// First we check if this is an CallbackEvent
+	case slackevents.CallbackEvent:
+		innerEvent := event.InnerEvent
+		// Yet Another Type switch on the actual Data to see if its an AppMentionEvent
+		switch ev := innerEvent.Data.(type) {
+		case *slackevents.AppMentionEvent:
+			// The application has been mentioned since this Event is a Mention event
+			err := handleEventMention(ev, api)
+			if err != nil {
+				log.Println("Error", err)
+				return err
+			}
+		}
+	default:
+		return errors.New("unsupported event type")
+	}
+	return nil
+}
+
+func handleEventMention(event *slackevents.AppMentionEvent, api *slack.Client) error {
+	// Grab the user name based on the ID of the one who mentioned the bot
+	user, err := api.GetUserInfo(event.User)
+	if err != nil {
+		return err
+	}
+	// Check if the user said Hello to the bot
+	text := strings.ToLower(event.Text)
+
+	// Create the attachment and assigned based on the message
+	attachment := slack.Attachment{}
+	// Add Some default context like user who mentioned the bot
+	attachment.Fields = []slack.AttachmentField{
+		{
+			Title: "Date",
+			Value: time.Now().String(),
+		}, {
+			Title: "Initializer",
+			Value: user.Name,
+		},
+	}
+	if strings.Contains(text, "hello") {
+		// Greet the user
+		attachment.Text = fmt.Sprintf("Hello %s", user.Name)
+		attachment.Pretext = "Greetings"
+		attachment.Color = "#4af030"
+	} else if strings.Contains(text, "btc") {
+		// Get BTC value
+		btc := getBtcValue()
+		attachment.Text = fmt.Sprintf("1 BTC equals to %s USD", btc)
+		attachment.Pretext = "As you wanted"
+		attachment.Color = "#ff8000"
+	} else {
+		// Send a message to the user
+		attachment.Text = fmt.Sprintf("How can I help you %s?", user.Name)
+		attachment.Pretext = "How can I be of service"
+		attachment.Color = "#3d3d3d"
+	}
+	// Send the message to the channel
+	// The Channel is available in the event message
+	_, _, err = api.PostMessage(event.Channel, slack.MsgOptionAttachments(attachment))
+	if err != nil {
+		return fmt.Errorf("failed to post message: %w", err)
+	}
+	return nil
+}
+
+func getBtcValue() string {
+	response, err := http.Get("https://cex.io/api/last_price/BTC/USD")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	var JsonResponse Response
+	body, _ := ioutil.ReadAll(response.Body)
+	err = json.Unmarshal(body, &JsonResponse)
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	return JsonResponse.LastPrice
+}
+
+type Response struct {
+	LastPrice string `json:"lprice"`
+	Currency1 string `json:"curr1"`
+	Currency2 string `json:"curr2"`
 }
