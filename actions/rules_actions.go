4bb1cde042443785601940a7797d1272a21d17b9
ateston1 <ateston@fi.uba.ar>
2022-12-11 19:00:18 -0300

    Add rules service

--- actions/rules_actions.go
+++ actions/rules_actions.go
@@ -1,1 +1,15 @@
 package actions
+
+import (
+	"github.com/slack-go/slack"
+)
+
+// HandleSetHigh Sets the high to then tell the user when the crypto value is higher
+func HandleSetHigh(splitedText []string, fields []slack.AttachmentField) slack.Attachment {
+	return slack.Attachment{}
+}
+
+// HandleSetLow Sets the low to then tell the user when the crypto value is lower
+func HandleSetLow(splitedText []string, fields []slack.AttachmentField) slack.Attachment {
+	return slack.Attachment{}
+}
